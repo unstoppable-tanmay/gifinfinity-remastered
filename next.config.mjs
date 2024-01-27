1560bbaea174a4a92b447de0be6aa9b036ed01ea
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2024-01-27 23:04:39 +0530

    resolving error with cors ✔

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,20 @@
 const nextConfig = {
     env: {
         GIPHY_API_KEY: "GlVGYHkr3WSBnllca54iNt0yFbjz7L65"
+    },
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "https://gifinfinity-remastered.vercel.app" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
     }
 };
 
