118819df40dfce83261b47e68da6e0ca29e5c92d
unstoppableTANMAY <iamunstoppableguy@gmail.com>
2024-01-27 23:18:29 +0530

    resolving error with cors ✔

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,9 @@ const nextConfig = {
     env: {
         GIPHY_API_KEY: "GlVGYHkr3WSBnllca54iNt0yFbjz7L65"
     },
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+    },
 };
 
 export default nextConfig;
